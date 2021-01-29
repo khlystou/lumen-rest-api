f0045a8d765a1d9d3a5433ef24ae2b8f94476f7c
Vasili Khlystou <khlystou@gmail.com>
2021-01-30 01:16:08 +0300

    Add migration

---
+++ database/migrations/2021_01_29_221054_create_clickid_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClickidTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clickid', function (Blueprint $table) {
+            $table->string('clickid', 40)->default(1);
+            $table->string('event', 40)->nullable();
+            $table->string('type', 40)->nullable();
+            $table->string('app_id', 40)->nullable();
+            $table->boolean('sell')->nullable();
+            $table->boolean('reg')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clickid');
+    }
+}
