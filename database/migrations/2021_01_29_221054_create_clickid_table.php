1c677f4646a47365dc453eb9416d40048f3cec7c
Vasili Khlystou <khlystou@gmail.com>
2021-01-30 01:25:33 +0300

    Update migration

--- database/migrations/2021_01_29_221054_create_clickid_table.php
+++ database/migrations/2021_01_29_221054_create_clickid_table.php
@@ -14,12 +14,14 @@ class CreateClickidTable extends Migration
     public function up()
     {
         Schema::create('clickid', function (Blueprint $table) {
+            $table->id();
             $table->string('clickid', 40)->default(1);
             $table->string('event', 40)->nullable();
             $table->string('type', 40)->nullable();
-            $table->string('app_id', 40)->nullable();
+            $table->string('app_id', 100)->nullable();
             $table->boolean('sell')->nullable();
             $table->boolean('reg')->nullable();
+            $table->timestamps();
         });
     }
 
