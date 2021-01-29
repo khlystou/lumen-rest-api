a4a0e21febfb0262e597fbade5fc965a2474289f
Vasili Khlystou <khlystou@gmail.com>
2021-01-29 23:47:15 +0300

    Update

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InfoController extends Controller
 {
     public function index(Request $request)
     {
-        return ['id' => $request->get('id')];
+        $request->validate([
+            'clickid' => ['required', 'string']
+        ]);
+
+        return DB::table('clickid')->where('clickid', $request->get('clickid'))->first();
     }
 
     public function store(Request $request)
     {
-        return ['data' => $request->all()];
+        $request->validate([
+            'clickid' => ['required', 'string'],
+            'event' => ['string'],
+            'type' => ['string'],
+            'app_id' => ['string'],
+            'sell' => ['integer'],
+            'reg' => ['integer']
+        ]);
+
+        return DB::table('clickid')->create(
+            $request->only(['clickid', 'event', 'type', 'app_id', 'sell', 'reg'])
+        );
     }
 }
