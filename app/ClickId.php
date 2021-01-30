5c59ff21228f260bbab175400501dcb2cdd1568d
Vasili Khlystou <khlystou@gmail.com>
2021-01-30 20:12:21 +0300

    Up

--- app/ClickId.php
+++ app/ClickId.php
@@ -8,6 +8,8 @@ class clickid extends Model
 {
     public $table = 'clickid';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'clickid', 'event', 'type', 'app_id', 'sell', 'reg'
     ];
