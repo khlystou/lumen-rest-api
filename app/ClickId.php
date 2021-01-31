eafc17de999c172909d0372f397de8d79aec14a7
Vasili Khlystou <khlystou@gmail.com>
2021-01-31 14:20:57 +0300

    Update model

--- app/ClickId.php
+++ app/ClickId.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class clickid extends Model
+class ClickId extends Model
 {
     protected $table = 'clickid';
     protected $primaryKey = 'clickid';
